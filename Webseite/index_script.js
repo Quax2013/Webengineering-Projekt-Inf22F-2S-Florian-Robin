0502da4e1f753be1d571407edd8e2eeb7ca0e608
Quax2013 <robin.alex.ernst@gmail.com>
2023-06-22 10:43:34 +0200

    Filter for API-response | Base-info on people

--- Webseite/index_script.js
+++ Webseite/index_script.js
@@ -21,13 +21,35 @@ function search() {
                 document.querySelector("#placeholder").innerHTML = "Keine Treffer";
                 return;
             }
+            document.querySelector("#contents").style.transition = "all 0.25s";
             document.querySelector("#contents").style.marginTop = "10vh";
             document.querySelector("#placeholder").innerHTML = "";
             for (let i of response.laureates) {
-                let x = document.createElement("p");
+                if (i.birth) {
+                    let resultDiv = document.createElement("div");
+                    let nameDiv = document.createElement("div");
+                    let infoDiv = document.createElement("div");
+                    let prizesDiv = document.createElement("div");
 
-                x.innerHTML = i.fullName.en;
-                document.querySelector("#placeholder").appendChild(x);
+                    resultDiv.className = "search-result-person";
+
+                    nameDiv.className = "person-name";
+                    nameDiv.innerHTML = i.fullName.en;
+
+                    infoDiv.className = "person-info";
+                    infoDiv.innerHTML = `* ${i.birth.date} - ${i.birth.place ? i.birth.place.locationString.en : ""}<br>+ ${i.death ? `${i.death.date} - ${i.death.place.locationString.en}` : "No Death"}`;
+
+                    prizesDiv.innerHTML = `- ${i.nobelPrizes[0].category.en}`;
+                    for (let x = 1; x < i.nobelPrizes.length; x++) {
+                        prizesDiv.innerHTML += `<br>- ${i.nobelPrizes[x].category.en}`;
+                    }
+
+                    resultDiv.appendChild(nameDiv);
+                    resultDiv.appendChild(infoDiv);
+                    resultDiv.appendChild(prizesDiv);
+
+                    document.querySelector("#placeholder").appendChild(resultDiv);
+                }
             }
 
         })
